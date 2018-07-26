a8862664e1e2ce328ded8a8f278d9811a6de9feb
milos996 <ivankovicmilos5@gmail.com>
2018-07-26 11:15:24 +0200

    #MIAE-9 Ispravljanje gresaka

--- zdravstvena_ustanova.js
+++ zdravstvena_ustanova.js
@@ -4,20 +4,6 @@ class Osoba {
         this.ime = ime;
         this.prezime = prezime;
     }
-
-    logovanje() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-        let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+'] kreirana osoba ' + this.ime + ' ' + this.prezime);
-
-    }
 }
 
 class Doktor extends Osoba {
@@ -27,241 +13,103 @@ class Doktor extends Osoba {
         this.specijalnost = spec;
         this.pacijenti = [];
         this.pregledi = [];
-        this.logovanje();
+        Utility.log(`Kreiran doktor: ${this.ime} ${this.prezime}`);
     }
 
     dodajPacijenta(pacijent) {
-
         this.pacijenti.push(pacijent);
     }
 
-    zakaziPregled(pacijentCurr, tip, vrijeme) {
-
-        let pregledOne = {
-
-            datum : vrijeme,
-            tipPregleda : tip,
-        };
-
-        this.pregledi.push(pregledOne);
-        pacijentCurr.zakazanPregled(pregledOne);
+    zakaziPregled(pacijent, pregled) {
+        this.pregledi.push(pregled);
+        pacijent.zakaziPregled(pregled);
     }
-
-    logovanje() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-	      let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+'] kreiran doktor ' + this.ime + ' ' + this.prezime);
-
-    }
-
 }
 
 class Pacijent extends Osoba {
 
-    constructor(ime, prezime, jmbg, brZdrav) {
+    constructor(ime, prezime, jmbg, brojKartona) {
         super(ime, prezime);
         this.jmbg = jmbg;
-        this.zdrav = brZdrav;
+        this.zdrav = brojKartona;
         this.pregledi = [];
-
-        this.logovanje();
+        this.doktor = null;
+        Utility.log(`Kreiran pacijent: ${this.ime} ${this.prezime}`);
     }
 
-    zakazanPregled(pregled) {
-
+    zakaziPregled(pregled) {
         this.pregledi.push(pregled);
     }
 
     dodajDoktora(doktor) {
-
         this.doktor = doktor;
-        this.logDodanDoktor(doktor);
+        Utility.log(`Dodan doktor: ${this.doktor.ime}  , pacijent: ${this.ime} ${this.prezime}`);
    }
 
     obaviPregled(tip) {
-
-        this.pregledi.map(preg => {
-            if(preg.tipPregleda.ime == tip) {
-                preg.tipPregleda.obaviPregled(this);
-            }
-        })
+        const pregled = this.pregledi.find(pregled => pregled.tip === tip);
+        pregled.obaviPregled(this);
     }
-
-
-    logDodanDoktor() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-        let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+'] dodan doktor ' + this.doktor.ime + ', pacijent :' + this.ime + ' ' + this.prezime );
-    }
-
-    logovanje() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-	      let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+'] kreiran pacijent ' + this.ime + ' ' + this.prezime);
-
-    }
-
 }
 
 class Pregled {
 
-    constructor(ime) {
+  constructor(tip, datum) {
+      this.tip = tip;
+      this.datum = datum;
+  }
 
-        this.ime = ime;
-    }
-
-    obaviPregled() {
-
-        console.log("Obavljanje pregleda");
-    }
-
-    log() {
-
-        console.log("Logovanje");
-    }
+  obaviPregled(pacijent) {
+      const result = pacijent.pregledi.filter(pregled => pregled != this);
+      pacijent.pregledi = result;
+      this.vrijednost = Math.floor(Math.random() * Math.floor(150));
+      this.vrijeme = Math.floor(Math.random() * Math.floor(23)) + 1;
+      Utility.log(`Obavljanje laboratorijskog pregleda: ${this.tip} / Vrijednost: ${this.vrijednost}, Vrijeme: ${this.vrijeme}`);
+  }
 }
 
 class KrvniPregled extends Pregled {
 
-    constructor(ime) {
-        super(ime);
+    constructor(datum) {
+        super("krvni pritisak", datum);
     }
 
     obaviPregled(pacijent) {
-
-        let index = -1;
-        for(let i = 0; i < pacijent.pregledi.length; i++) {
-
-            if(pacijent.pregledi.tipPregleda == this) {
-                index = i;
-            }
-        }
-
-        pacijent.pregledi.splice(index, 1);
-        //Ovdje moze neka rand funkcija ili neko tacno mjerenje
-        this.gornjaV = 100;
-        this.donjaV = 10;
-        this.puls = 90;
-
-        this.log();
+        const result = pacijent.pregledi.filter(pregled => pregled != this);
+        pacijent.pregledi = result;
+        this.gornjaV = Math.floor(Math.random() * Math.floor(50)) + 50;
+        this.donjaV = Math.floor(Math.random() * Math.floor(50));
+        this.puls = Math.floor(Math.random() * Math.floor(50));
+        Utility.log(`Obavljanje laboratorijskog pregleda: ${this.tip}/ Gornja vrijednost: ${this.gornjaV}, donja vrijednost: ${this.donjaV}, puls : ${this.puls}`);
     }
+}
 
-    log() {
+class Utility{
 
+    static getDate() {
         let today = new Date();
         let dd = today.getDate();
         let mm = today.getMonth() + 1;
         let year = today.getFullYear();
         let curHour = today.getHours();
         let curMinute = today.getMinutes();
+        const date = `${dd}.${mm}.${year} ${curHour}:${curMinute}`;
 
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+']' + 'Krvni pregled / gornja vrijednost : ' + this.gornjaV + ', donja vrijednost :' + this.donjaV + ', puls :' + this.puls + '\n');
+        return date;
+    }
+
+    static log(output) {
+        const date = Utility.getDate();
+        console.log(`[${date}] ${output}`);
     }
 }
 
-class NivoSecera extends Pregled {
-
-    constructor(ime) {
-        super(ime);
-    }
-
-    obaviPregled(pacijent) {
-
-        let index = -1;
-        for(let i = 0; i < pacijent.pregledi.length; i++) {
-
-            if(pacijent.pregledi.tipPregleda == this) {
-                index = i;
-            }
-        }
-
-        pacijent.pregledi.splice(index, 1);
-        //Ovdje moze neka rand funkcija ili neko tacno mjerenje
-        this.vrijednost = 4;
-        this.vrijeme = '19:00';
-
-        this.log();
-    }
-
-    log() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-        let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+']' + 'Nivo secera u krvi / vrijednost : ' + this.vrijednost + ', vrijeme poslednjeg obroka :' + this.vrijeme + '\n');
-    }
-}
-
-class NivoHolesterola extends Pregled {
-
-    constructor(ime) {
-        super(ime);
-    }
-
-    obaviPregled(pacijent) {
-
-        let index = -1;
-        for(let i = 0; i < pacijent.pregledi.length; i++) {
-
-            if(pacijent.pregledi.tipPregleda == this) {
-                index = i;
-            }
-        }
-
-        pacijent.pregledi.splice(index, 1);
-        //Ovdje moze neka rand funkcija ili neko tacno mjerenje
-        this.vrijednost = 4;
-        this.vrijeme = '19:00';
-
-        this.log();
-    }
-
-    log() {
-
-        let today = new Date();
-        let dd = today.getDate();
-        let mm = today.getMonth() + 1;
-        let year = today.getFullYear();
-        let curHour = today.getHours();
-        let curMinute = today.getMinutes();
-
-        let date = dd + '.' + mm + '.' + year + ' ' + curHour + ':' + curMinute;
-        console.log('['+date+']' + 'Nivo holesterola u krvi / vrijednost : ' + this.vrijednost + ', vrijeme poslednjeg obroka :' + this.vrijeme + '\n');
-    }
-}
 
 //Test
 let doktor = new Doktor('Milan', 'Milanovic', 'hirurg');
 let  pacijent = new Pacijent('Dragan', 'Ivanovic', '1241250821500', '0001');
 pacijent.dodajDoktora(doktor);
-doktor.zakaziPregled(pacijent, new NivoSecera('nivo secera'), ' 3.10.2018 14:25');
-doktor.zakaziPregled(pacijent, new KrvniPregled('krvni pritisak'), ' 3.10.2018 14:25');
+doktor.zakaziPregled(pacijent, new Pregled('nivo secera', '3.10.2018 14:25'));
+doktor.zakaziPregled(pacijent, new KrvniPregled('3.10.2018 14:25'));
 pacijent.obaviPregled('krvni pritisak');
 pacijent.obaviPregled('nivo secera');
